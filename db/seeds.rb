d2e7138b90032b7f1aaf0658f5783b8a5c7c4bbb
Eranda  Irushan <eirushan@icptechno.com>
2023-02-22 15:35:29 +0530

    Add deivse gem and user authentication and user views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: "erandairushan98@gmail.com", password: "password", password_confirmation: "password")
+
 10.times do |i|
-    Post.create(title: "Lorem ipsum #{i}", body: "Lorem ipsum dolor sit amet, consectetur adipiscing #{i}")
+    Post.create(title: "Lorem ipsum #{i}", body: "Lorem ipsum dolor sit amet, consectetur adipiscing #{i}", user_id: User.first.id)
+
 end
