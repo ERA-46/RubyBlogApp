1b9e3dfb0ce456bcd44e44c317433328cae59b6c
Eranda  Irushan <eirushan@icptechno.com>
2023-02-23 11:48:26 +0530

    Added reply section to blogs with edit function with stimulus

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
     validates :title, presence: true, length: {minimum:5, maximum:50}
     validates :body, presence: true, length: {minimum:10, maximum:1000}
-    belongs_to :user 
+    belongs_to :user
+    #after_destroy {notify admin of bad behavior}
+    has_many :comments, dependent: :destroy
 end
